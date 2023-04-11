606d557fc76b7c8cd81e6b40e4c0f7603b0f8ad8
lkb17 <102708361+Lotar17@users.noreply.github.com>
2023-04-10 23:00:21 -0300

    Realizar Labs01 de la Unidad 1- Capitulo 1 hasta ejercicio 2

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Labs01/Decision/Decision con CASE/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decision_con_CASE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputTexto;
+            Console.WriteLine("INGRESE UN TEXTO (CASE)");
+            Console.WriteLine();
+            inputTexto = Console.ReadLine();
+            Console.WriteLine();
+            if (inputTexto.Length != 0)
+            {
+                Console.WriteLine("1- Mostrar la frase ingresada en mayúsculas");
+                Console.WriteLine("2- Mostrar la frase ingresada en minúsculas");
+                Console.WriteLine("3- Contar los caracteres de la frase ingresada");
+                Console.WriteLine();
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                switch (opcion.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine();
+                        Console.WriteLine(inputTexto.ToUpper());
+                        Console.WriteLine("pulse cualquier tecla para finalizar el programa...");
+                        Console.ReadKey();
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine();
+                        Console.WriteLine(inputTexto.ToLower());
+                        Console.WriteLine("pulse cualquier tecla para finalizar el programa...");
+                        Console.ReadKey();
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine();
+                        Console.WriteLine("EL TEXTO TIENE ESTA CANTIDAD DE CARACTERES:");
+                        Console.WriteLine(inputTexto.Length);
+                        Console.WriteLine("pulse cualquier tecla para finalizar el programa...");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("NO ELIGIÓ NINGUNA DE LAS OPCIONES ESPERADAS, FIN DE LA APLICACIÓN");
+                        Console.WriteLine("pulse cualquier tecla para finalizar el programa...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("NO INGRESÓ NINGUN TEXTO, FIN DEL PROGRAMA");
+                Console.WriteLine("pulse cualquier tecla para finalizar el programa...");
+                Console.ReadKey();
+            }
+        
+        }
+    }
+}

3b88d787dd2eb06de6e644ae7646de3feda4c023
lkb17 <102708361+Lotar17@users.noreply.github.com>
2023-04-11 21:53:46 -0300

    Realizar ejercicio 3 Labs01 Unidad 2 Capitulo 01

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Labs01/Decision/Iteracion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] palabras = new string[5];
+            int cantIteraciones = 5;
+            for (int i=0; i< cantIteraciones; i++)
+            {
+                Console.WriteLine("INGRESE UNA PALABRA PARA LA POSICION " + (i+1));
+                palabras[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("POSICION     PALABRA");
+
+            for(int i=cantIteraciones-1; i >= 0; i--)
+            {
+                Console.WriteLine((i+1) + "             " + palabras[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PULSE UNA TECLA PARA FINALIZAR EL PROGRAMA...");
+            Console.ReadKey();
+        }
+    }
+}
